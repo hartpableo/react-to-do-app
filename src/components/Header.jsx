dd4211c37b8a8137581739d26c058ca309cca9c8
Hart <pableoh@gmail.com>
2023-02-12 12:51:17 +0800

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header role='banner'>
+            <div className="container text-center">
+                <h1 className="title text-[70px] font-thin">my todos</h1>
+            </div>
+        </header>
+    )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      primaryFont: "'Raleway', sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
