34f5b575589bd60144abcfce52f25cf827c10d35
Hart <pableoh@gmail.com>
2023-02-13 16:24:14 +0800

    progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header role='banner'>
             <div className="container text-center">
-                <h1 className="title text-[70px] text-gray-500 font-thin">my todos</h1>
+                <h1 className="title text-[70px] text-gray-500 font-thin mb-3">my todos</h1>
             </div>
         </header>
     )
